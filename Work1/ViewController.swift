76eedb507032d76fda1aca2705e8784de75ecf47
KAKAO <KAKAO@kakaoui-macbook-air-3.local>
2016-10-21 19:01:22 +0900

    Initial Commit

---
+++ Work1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Work1
+//
+//  Created by KAKAO on 2016. 10. 21..
+//  Copyright © 2016년 KAKAO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
