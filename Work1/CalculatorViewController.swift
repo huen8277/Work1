11301b3c72e90b1aed656484a3ef37bf58408287
david <huen8277@gmail.com>
2016-11-17 12:58:19 +0900

    op 버튼 이후 disNum을 storedNum으로 해서 =을 n번 눌려도 결과값이 정상적으로 나오게 하는 작업

--- Work1/CalculatorViewController.swift
+++ Work1/CalculatorViewController.swift
@@ -17,6 +17,7 @@ class CalculatorViewController: UIViewController {
     var storedNum: String?
     var storedOp: String?
     
+    var firstCal: Bool = false // bool 타입 true, false 구분
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +36,7 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         let num: Int = sender.tag
-        var curNum = getDisplayedNumber()
+        var curNum = getDisplayedNumber() // 화면에 디스플레이되던 값이 curNum으로 들어간다.
         //첫 숫자가 0인 경우 처리 로직.
         if curNum == "0"{
             curNum = ""
@@ -52,7 +53,8 @@ class CalculatorViewController: UIViewController {
     func calculate() -> String {
         let curNum = getDisplayedNumber()
         var calNum = curNum
-        
+        //옵셔널 타입을 일반 String 타입으로 변환
+        //storedNum이 nil일 경우 if문 조건에 부합하지 않아 해당 조건문을 실행 하지 않는다.
         if let storedNum = storedNum, let storedOp = storedOp {
             if storedOp == "+" {
                 calNum = String(Int(storedNum)! + Int(curNum)!)
@@ -76,15 +78,31 @@ class CalculatorViewController: UIViewController {
         // 사칙연산
         numberDisplayLabel.text = calculate()
         storedOp = op
-        storedNum = numberDisplayLabel.text
+        storedNum = numberDisplayLabel.text // op를 누르면서 curNum이 storedNum으로 들어간다. 새로 입력 받을 예정인 Num이 curNum으로 들어갈 거시다. 흠...
+        firstCal = true // 처음계산했다. true
     }
     
     @IBAction func equalButtonPressed(_ sender: UIButton) {
         let equal = sender.title(for: .normal)
         print("\(equal)pressed")
         //= 로직
+        // 추가코드
+        var tempNum: String?
+        if firstCal {
+            tempNum = getDisplayedNumber()
+        }
+        
         numberDisplayLabel.text = calculate()
-        storedOp = nil
+        
+        if let tempNum = tempNum {
+            storedNum = tempNum
+        }
+        
+        
+        
+        firstCal = false
+        // = 을 누르면서 새로 입력받은 num에 있던 숫자와 storedNum이 더해지고 그거시 calNum으로 저장되어 출력될것이다.
+        // 또 =을 누르면 calNum이 curNumd으로가고 젤 처음에 입력 되엇던 num이 storedNum이라 =을 계속 눌려도 뒤에 누른 num 값이 아닌 처음에 누른 num으로 계속 값이 더해진다.
     }
     
     @IBAction func acButtonPressed(_ sender: UIButton) {
